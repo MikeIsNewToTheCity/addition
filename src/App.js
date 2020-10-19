7793b51cb386dd1a96afec330a6ad459d560230a
MikeIsNewToTheCity <mikevilleneuve@gmail.com>
2020-10-19 18:39:48 -0500

    Got Addition.js up and running for basic addition and cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Addition from './addition';
+import Addition from './Addition';
 
 const App = () => {
-  return (
-    <>
-      <Addition numberOne={4} numberTwo={4} />
-      <Addition numberOne={7} numberTwo={-1} />
-      <Addition numberOne={-20} numberTwo={27} />
-      <Addition numberOne={2.1} numberTwo={1.9} />
-      <Addition numberOne={1} numberTwo={2} />
-      <Addition numberOne={100} numberTwo={-100} />
-      <Addition numberOne={534} numberTwo={-525} />
-    </>
-  );
+    return (
+        <>
+            <Addition numberOne={ 4 } numberTwo={ 4 } />
+            <Addition numberOne={ 7 } numberTwo={ -1 } />
+            <Addition numberOne={ -20 } numberTwo={ 27 } />
+            <Addition numberOne={ 3.1 } numberTwo={ 1.9 } />
+            <Addition numberOne={ 1 } numberTwo={ 2 } />
+            <Addition numberOne={ 100 } numberTwo={ -100 } />
+            <Addition numberOne={ 534 } numberTwo={ -525 } />
+        </>
+    );
 }
 
 export default App;
---
+++ src/addition.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Addition = (props) => {
+    return (
+        <>
+        <p>
+        { props.numberOne } + { props.numberTwo } = { props.numberOne + props.numberTwo }
+        </p>
+        </>
+    )
+}
+
+export default Addition;
